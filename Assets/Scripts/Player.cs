3a3ec6231d7a3f98e1f83a2773d81cc58f29b7dc
George Toliashvili <thebrainycraft@gmail.com>
2019-01-26 01:56:05 +0200

    Pick up and drop on tables

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public static bool actionButtonPressed = false;
+	public static bool hasItem = false;
+
+	[SerializeField] GameObject hands = null;
+ 
+	[SerializeField] float speed = 10f;
+
+	private Item item = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+		float realTimeSpeed = Time.deltaTime * speed;
+		transform.Translate(movement * realTimeSpeed);
+		hands.transform.position = transform.position;
+
+		if(hands.GetComponentInChildren<Item>())
+		{
+			hasItem = true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.F))
+		{
+			actionButtonPressed = true;
+		}
+	}
+
+	private void OnCollisionStay(Collision collision)
+	{
+		Table table = collision.gameObject.GetComponent<Table>();
+
+		if (table && actionButtonPressed && !table.slotIsEmpty && !hasItem)
+		{
+			item = collision.gameObject.GetComponentInChildren<Item>();
+			item.transform.SetParent(hands.transform);
+			item.transform.position = new Vector3 (hands.transform.position.x + 1f, hands.transform.position.y + 0.1f, hands.transform.position.z);
+			table.slotIsEmpty = true;
+			hasItem = true;
+			actionButtonPressed = false;
+		}
+		else if (table && table.slotIsEmpty && actionButtonPressed && hasItem)
+		{
+			item.transform.SetParent(table.itemSlot.transform);
+			float itemSize = item.transform.localScale.x / 2;
+			item.transform.position = new Vector3(table.itemSlot.transform.position.x, table.itemSlot.transform.position.y + itemSize, table.itemSlot.transform.position.z);
+			table.slotIsEmpty = false;
+			hasItem = false;
+			actionButtonPressed = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Table.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour {
+
+	public bool slotIsEmpty = true;
+
+	public GameObject itemSlot = null;
+
+	// Use this for initialization
+	void Start () {
+		if (GetComponentInChildren<Item>())
+		{
+			slotIsEmpty = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+	//private void OnCollisionStay(Collision collision)
+	//{
+	//	if (Player.actionButtonPressed && collision.gameObject.GetComponent<Player>() && Player.hasItem && slotIsEmpty)
+	//	{
+	//		Item item = collision.gameObject.GetComponentInChildren<Item>();
+	//		item.transform.parent = itemSlot.transform;
+	//		item.transform.position = itemSlot.transform.position;
+	//		slotIsEmpty = false;
+	//	}
+	//}
+}
