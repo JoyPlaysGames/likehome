1f4ab38ae6b4edb07e63d0e59d3e0c86b68178a4
Joy <davis.toliasvili@gmail.com>
2019-01-26 04:23:57 +0200

    Snake, rat, spider fbx

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour {
 
 	public static bool actionButtonPressed = false;
 	public static bool hasItem = false;
+	private bool animationPick = false;
+	private bool animationPlace = false;
 
 	[SerializeField] GameObject hands = null;
  
@@ -43,37 +45,36 @@ public class Player : MonoBehaviour {
 		{
 			hasItem = true;
 		}
-
-		if (Input.GetKeyDown(KeyCode.F))
-		{
-			actionButtonPressed = true;
-		}
+		
 	}
 
 	private void OnTriggerStay(Collider other)
 	{
 		Table table = other.gameObject.GetComponent<Table>();
 
-		if (table && actionButtonPressed && !table.slotIsEmpty && !hasItem)
+		if (Input.GetKeyDown(KeyCode.F))
 		{
-			item = other.gameObject.GetComponentInChildren<Item>();
-			item.transform.SetParent(hands.transform);
-			item.transform.position = new Vector3(hands.transform.position.x, hands.transform.position.y, hands.transform.position.z);
-			table.slotIsEmpty = true;
-			hasItem = true;
-			actionButtonPressed = false;
-			playerAnimator.SetTrigger("PickItem");
-		}
-		else if (table && table.slotIsEmpty && actionButtonPressed && hasItem)
-		{
-			item.transform.SetParent(table.itemSlot.transform);
-			item.transform.rotation = Quaternion.identity;
-			float itemSize = item.transform.localScale.x / 2;
-			item.transform.position = new Vector3(table.itemSlot.transform.position.x, table.itemSlot.transform.position.y + itemSize, table.itemSlot.transform.position.z);
-			table.slotIsEmpty = false;
-			hasItem = false;
-			actionButtonPressed = false;
-			playerAnimator.SetTrigger("PlaceItem");
+			if (table && !table.slotIsEmpty && !hasItem)
+			{
+				item = other.gameObject.GetComponentInChildren<Item>();
+				item.transform.SetParent(hands.transform);
+				item.transform.position = new Vector3(hands.transform.position.x, hands.transform.position.y, hands.transform.position.z);
+				table.slotIsEmpty = true;
+				hasItem = true;
+				playerAnimator.SetTrigger("PickItem");
+				Debug.Log("Item Picked");
+			}
+			else if (table && table.slotIsEmpty && hasItem)
+			{
+				item.transform.SetParent(table.itemSlot.transform);
+				item.transform.rotation = Quaternion.identity;
+				float itemSize = item.transform.localScale.x / 2;
+				item.transform.position = new Vector3(table.itemSlot.transform.position.x, table.itemSlot.transform.position.y + itemSize, table.itemSlot.transform.position.z);
+				table.slotIsEmpty = false;
+				hasItem = false;
+				playerAnimator.SetTrigger("PlaceItem");
+				Debug.Log("Item Placed");
+			}
 		}
 	}
 }
